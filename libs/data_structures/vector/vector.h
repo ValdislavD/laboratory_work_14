0c4013770fbc5d6ce184e0fedacf0f94c485cb25
VladislavD <vladislavdmuhov792@gmail.com>
2024-03-03 22:06:17 +0300

    memory usage of vector

--- libs/data_structures/vector/vector.h
+++ libs/data_structures/vector/vector.h
@@ -1,5 +1,31 @@
 #ifndef UNTITLED17_VECTOR_H
 #define UNTITLED17_VECTOR_H
 
+#include <stdbool.h>
+#include <stdio.h>
+#include <assert.h>
+
+typedef struct vector {
+    int *data; // указатель на элементы вектора
+    size_t size; // размер вектора
+    size_t capacity; // вместимость вектора
+} vector;
+
+// возвращает структуру-дескриптор вектор из n значений
+vector createVector(size_t n);
+
+// изменяет количество памяти на newCapacity,
+// выделенное под хранение элементов вектора v
+void reserve(vector *v, size_t newCapacity);
+
+// удаляет элементы из вектора v,
+// но не освобождает выделенную память
+void clear(vector *v);
+
+// освобождает память, выделенную под неиспользуемые элементы вектора v
+void shrinkToFit(vector *v);
+
+// освобождает память, выделенную вектору v
+void deleteVector(vector *v);
 
 #endif //UNTITLED17_VECTOR_H
